2f07384c1ab212d1f22fd37cb56aa17298abfbf7
MataPutos <juanpasias@gmail.com>
2023-02-25 06:20:45 -0600

    con todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CuadroComponent } from './cuadro/cuadro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuadroComponent
   ],
   imports: [
     BrowserModule
